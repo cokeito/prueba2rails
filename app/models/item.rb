ae724652912050bfe868087a419c35fb2ce948e6
cokeito <coke@mta.cl>
2016-10-28 20:26:06 -0300

    5th commits - scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
+  scope :size_x, -> (n) { where(:size => n) }
+  scope :no_users, -> { where(:user_id => nil) }
+
 end
