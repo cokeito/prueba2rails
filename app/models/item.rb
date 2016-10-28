5aedc1c8ca0f2c1e6b64196baef452e218c24797
cokeito <coke@mta.cl>
2016-10-28 20:44:26 -0300

    sixth commit - delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
   scope :size_x, -> (n) { where(:size => n) }
   scope :no_users, -> { where(:user_id => nil) }
 
+
+  after_destroy :remove_category_if_empty
+
+  def remove_category_if_empty
+  	if self.category.items.count.zero?
+  		self.category.destroy
+  	end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-	has_many :items
+	has_many :items, dependent: :destroy
+	
 	validates_presence_of :name
 	validates :email, uniqueness: true
 
