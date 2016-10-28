9e17193cb7f5f8f1fdfda4f9387c82d343d29613
cokeito <coke@mta.cl>
2016-10-28 20:16:47 -0300

    2nd commit fix, missed capitalize on name , sorry :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 	has_many :items
 	validates_presence_of :name
 	validates :email, uniqueness: true
+
+	before_validation :set_first_capitalize
+
+	def set_first_capitalize
+		self.name = self.name.to_s.capitalize
+	end
+
 end
